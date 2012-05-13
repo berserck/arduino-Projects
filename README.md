3a4715583d103ccaa3b11a0a022d5104e37cdec0
Pedro Parracho <pedro.parracho@gmail.com>
2012-05-13 12:41:51 +0200

    Remove some commented lines

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ arduino-Projects
 ================
 
 Repository with code for arduino
+
+- Scroll program:
+Hardware
+=========
+- LED module 
+- IR receiver
+
--- scroll/scroll.ino
+++ scroll/scroll.ino
@@ -1,47 +1,25 @@
 #include <TM1638.h>
-int TM168_STR = 9; /* brown  */
-int TM168_DAT = 8; /* orange */
-int TM168_CLK = 7; /* green  */
-
-int IR_DAT = 2;    /* brown */
-
-int RLY_DAT = 4;   /* yellow */
+const int TM168_STR = 9; /* brown  */
+const int TM168_DAT = 8; /* orange */
+const int TM168_CLK = 7; /* green  */
+const int IR_DAT = 2;    /* brown  */
+const int RLY_DAT = 4;   /* yellow */
 
 //TM1638(byte dataPin, byte clockPin, byte strobePin, boolean activateDisplay, byte intensity);
 // 9=brown, 8=orange, 7=green 
 TM1638 module(TM168_DAT, TM168_STR , TM168_CLK, true, 4);
 // counters to make things slower...
-int count=0;
-int wait=0;
-int pos=0;
+int count, wait, pos, color, blinkTimer=0;
 String str="LENkA";
 boolean start=false;
 //LED colors
 int colors[4]={
   0xFF, 0xFF00, 0xFFFF, 0x0000};
-int color=0;
-int blinkTimer=0;
-
 boolean turn=false;
 int lumi = 5;
-//IR
-//in pin for IR
-const int inPin = 2;
-
-/*
-  //VARIABLES
- count=0;
- wait=0;
- pos=0;
- str="LENkA";
- start=false;
- blinkTimer=0;
- color=0;
- turn=false;
- */
 
 // function to clear the line before pos
-// default=8 clears teh whole line
+// default=8 clears the whole line
 void clearLine(unsigned int pos=8)
 {
   for(int i=0; i<pos; i++) {
@@ -51,8 +29,8 @@ void clearLine(unsigned int pos=8)
 
 int setIntensity(int lvl)
 {
-  if(lvl > 7) lvl = 7;
-  if(lvl < 0) lvl = 0;
+  if(lvl >= 7) lvl = 7;
+  if(lvl <= 0) lvl = 0;
   module.setupDisplay(true, lvl);
   module.setDisplayToDecNumber(lvl,0,true); 
   delay(500);
@@ -63,14 +41,14 @@ int setIntensity(int lvl)
 void setup() {
   // HARDWARE
   module.setDisplayToDecNumber(0,0,true);
-  // module.setupDisplay(true, 4);
-  pinMode(inPin, INPUT);
+  pinMode(IR_DAT, INPUT);
+  pinMode(RLY_DAT, OUTPUT);
 }
 
 void loop() {
+  // read the button value
   byte keys = module.getButtons();
-
-  if (keys == 0x1 || digitalRead(inPin) == LOW){
+  if (keys == 0x1 || digitalRead(IR_DAT) == LOW){
     start = !start; // start scrolling or pause
     delay(500);
   }
@@ -90,8 +68,7 @@ void loop() {
 
   if (start) {
     module.setLEDs((0x1 << pos) << (turn ? 8:0));
-    wait++;
-    //module.setDisplayToDecNumber(0x1 << pos,0);  
+    wait++; 
     module.setDisplayToString( str, 0, pos);
     clearLine(pos);
 
@@ -116,7 +93,6 @@ void loop() {
       module.setLEDs(colors[color]);
     }
   }
-  //module.setLEDs(((keys & 0xF0) << 8) | (keys & 0x0F));
 }
 
 
